c2d49c223cc6417bcec03c7a3b250e24045cb010
PKMDroid <princekibali222@gmail.com>
2025-12-01 07:27:39 +0300

    changes

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,13 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# Names: capitalized, can include hyphens/apostrophes, 1+ words
+name = r"^[A-Z][a-zA-Z'’-]*(?: [A-Z][a-zA-Z'’-]*)*$"
 name_regex = re.compile(name)
 
-phone_number = r""
+# Phone numbers: unchanged
+phone_number = r"^(\+?\d{1,2}\s?)?(\(?\d{3}\)?)[\s.-]?\d{3}[\s.-]?\d{4}$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+# Emails: must start with a letter
+email_address = r"^[A-Za-z][A-Za-z0-9._%+-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
 email_regex = re.compile(email_address)
